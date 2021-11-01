4f40c7b1e7dce5191e89c5d5c78465d3fa2951e1
hieubkap2000 <hieubkap2000@gmail.com>
2021-11-01 14:22:53 +0700

    Bai11BubbleSort

---
+++ Bai11BubbleSort.js
@@ -0,0 +1,26 @@
+const listProduct = [
+  { name: "CPU", price: 750, quality: 10, categoryId: 1 },
+  { name: "RAM", price: 50, quality: 2, categoryId: 2 },
+  { name: "HDD", price: 70, quality: 1, categoryId: 2 },
+  { name: "Main", price: 400, quality: 3, categoryId: 1 },
+  { name: "Keyboard", price: 30, quality: 8, categoryId: 4 },
+  { name: "Mouse", price: 25, quality: 50, categoryId: 4 },
+  { name: "VGA", price: 60, quality: 35, categoryId: 3 },
+  { name: "Monitor", price: 120, quality: 28, categoryId: 2 },
+  { name: "Case", price: 120, quality: 28, categoryId: 5 },
+];
+
+function sortByPrice(listProduct) {
+  for (var i = 0; i < listProduct.length; i++) {
+    for (let j = i + 1; j < listProduct.length; j++) {
+      if (listProduct[i].price > listProduct[j].price) {
+        var tmp = listProduct[i];
+        listProduct[i] = listProduct[j];
+        listProduct[j] = tmp;
+      }
+    }
+  }
+  return listProduct;
+}
+
+console.log(sortByPrice(listProduct));
